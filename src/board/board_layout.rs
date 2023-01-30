2894b4a53451e5ecc90407a1035a881f1169afff
Frederick Watkins <freddiewatkins6@gmail.com>
2023-01-30 20:22:07 +0000

    Fixed DEFAULT_BOARD being rotated by 90 degrees

--- src/board/board_layout.rs
+++ src/board/board_layout.rs
@@ -6,18 +6,18 @@ use array2d::Array2D;
 lazy_static! {
     /// Default chess board layout. 0, 0 is A1 etc
     pub static ref DEFAULT_BOARD: Array2D<Option<Piece>> = {
-        Array2D::from_row_major(&[
+        Array2D::from_column_major(&[
             Some(Piece::new(White, Rook)),   Some(Piece::new(White, Knight)), Some(Piece::new(White, Bishop)), Some(Piece::new(White, Queen)),  Some(Piece::new(White, King)),   Some(Piece::new(White, Bishop)), Some(Piece::new(White, Knight)), Some(Piece::new(White, Rook)),
         
             Some(Piece::new(White, Pawn)),   Some(Piece::new(White, Pawn)),   Some(Piece::new(White, Pawn)),   Some(Piece::new(White, Pawn)),   Some(Piece::new(White, Pawn)),   Some(Piece::new(White, Pawn)),   Some(Piece::new(White, Pawn)),   Some(Piece::new(White, Pawn)),
         
-            None,                  None,                  None,                  None,                  None,                  None,                  None,                  None,
+            None,                            None,                            None,                            None,                            None,                            None,                            None,                            None,
         
-            None,                  None,                  None,                  None,                  None,                  None,                  None,                  None,
+            None,                            None,                            None,                            None,                            None,                            None,                            None,                            None,
         
-            None,                  None,                  None,                  None,                  None,                  None,                  None,                  None,
+            None,                            None,                            None,                            None,                            None,                            None,                            None,                            None,
         
-            None,                  None,                  None,                  None,                  None,                  None,                  None,                  None,
+            None,                            None,                            None,                            None,                            None,                            None,                            None,                            None,
         
             Some(Piece::new(Black, Pawn)),   Some(Piece::new(Black, Pawn)),   Some(Piece::new(Black, Pawn)),   Some(Piece::new(Black, Pawn)),   Some(Piece::new(Black, Pawn)),   Some(Piece::new(Black, Pawn)),   Some(Piece::new(Black, Pawn)),   Some(Piece::new(Black, Pawn)),
         
