dfd9ba3114a2fe41b64ec6acd7d1215468861408
Frederick Watkins <freddiewatkins6@gmail.com>
2023-02-06 23:18:17 +0000

    Added test printout of move error in chess_cli

--- chess_cli/src/main.rs
+++ chess_cli/src/main.rs
@@ -1,10 +1,15 @@
 use chess_lib::board;
 
 fn main() {
-    let b = board::Board::new();
+    let mut b = board::Board::new();
     eprintln!(
         "{}",
         b.check_positions(board::Position::new(3, 3).unwrap())
+            .unwrap_err()
+    );
+    eprintln!(
+        "{}",
+        b.move_piece(board::Position::new(0, 0).unwrap(), board::Position::new(0, 7).unwrap())
             .unwrap_err()
     );
     eprintln!("{}", board::Position::new(8, 1).unwrap_err())
