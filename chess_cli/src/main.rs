18d092d9901b5da5e28781ff474d37771682616d
Frederick Watkins <freddiewatkins6@gmail.com>
2023-02-07 18:58:39 +0000

    Moved current board implementation into board::mailbox to allow for other implementations in future

--- chess_cli/src/main.rs
+++ chess_cli/src/main.rs
@@ -1,16 +1,5 @@
-use chess_lib::board;
+
 
 fn main() {
-    let mut b = board::Board::new();
-    eprintln!(
-        "{}",
-        b.check_positions(board::Position::new(3, 3).unwrap())
-            .unwrap_err()
-    );
-    eprintln!(
-        "{}",
-        b.move_piece(board::Position::new(0, 0).unwrap(), board::Position::new(0, 7).unwrap())
-            .unwrap_err()
-    );
-    eprintln!("{}", board::Position::new(8, 1).unwrap_err())
+    
 }
